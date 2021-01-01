33a2fa233215699fe64b081764535e961f45adbe
Ajay Singh <daltavida@gmail.com>
2021-01-01 18:07:00 +1030

    Use API Features module in tour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.aliasFeatured = (req, res, next) => {
   req.query.limit = '5';
@@ -6,59 +7,6 @@ exports.aliasFeatured = (req, res, next) => {
   req.query.fields = 'name, price, ratingsAverage, summary, difficulty';
   next();
 };
-
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-
-  filter() {
-    const queryObject = { ...this.queryString };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-
-    excludedFields.forEach((el) => delete queryObject[el]);
-
-    let queryStr = JSON.stringify(queryObject);
-    queryStr = JSON.parse(
-      queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
-    );
-
-    this.query = this.query.find(queryStr);
-
-    return this;
-  }
-
-  sort() {
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort('-createdAt');
-    }
-
-    return this;
-  }
-
-  limit() {
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
-      this.query = this.query.select(fields);
-    } else {
-      this.query = this.query.select('-__v');
-    }
-    return this;
-  }
-
-  paginate() {
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    this.query = this.query.skip(skip).limit(limit);
-    return this;
-  }
-}
 
 exports.getAllTours = async (req, res) => {
   try {
