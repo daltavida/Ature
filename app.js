3297932d1179c0eaaebcf5c4564c29df62532902
Ajay Singh <daltavida@gmail.com>
2021-11-12 18:07:00 +1030

    Use middleware for POST request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -10,11 +11,14 @@ const tours = JSON.parse(
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
+    results: tours.length,
     data: {
       tours,
     },
   });
 });
+
+app.post('/api/v1/tours', (req, res) => {});
 
 const port = 3000;
 app.listen(port, '127.0.0.1', () => {
