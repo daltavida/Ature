23a3ff87000a360ad0eee91fe7cef9de87a2eb5a
Ajay Singh <daltavida@gmail.com>
2021-11-15 18:07:00 +1030

    Create GET request for tour by ID

--- app.js
+++ app.js
@@ -18,6 +18,15 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: tour,
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newID = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newID }, req.body);
