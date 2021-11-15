98f3466bcc33d42fdacc83321d08aaa820069fc4
Ajay Singh <daltavida@gmail.com>
2021-11-15 18:07:00 +1030

    Add condition for invalid ID

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ app.get('/api/v1/tours', (req, res) => {
 app.get('/api/v1/tours/:id', (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
   res.status(200).json({
     status: 'success',
     data: tour,
