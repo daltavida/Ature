476bc9a9008230980f9e25ac4640365939586e1e
Ajay Singh <daltavida@gmail.com>
2021-11-17 18:07:00 +1030

    Create endpoint for deleting tour by id

--- app.js
+++ app.js
@@ -70,6 +70,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 >= tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 app.listen(port, '127.0.0.1', () => {
   console.log(`App running on port ${port}`);
