f7b968af8693b7da5b90ebeb51b70625b5b82ee8
Ajay Singh <daltavida@gmail.com>
2021-11-18 18:07:00 +1030

    Create function to DELETE tour by id

--- app.js
+++ app.js
@@ -70,12 +70,7 @@ const updateTourByID = (req, res) => {
   });
 };
 
-app.get('/api/v1/tours', getAllTours);
-app.get('/api/v1/tours/:id', getTourByID);
-app.post('/api/v1/tours', createTour);
-app.patch('/api/v1/tours/:id', updateTourByID);
-
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTourByID = (req, res) => {
   if (req.params.id * 1 >= tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -87,7 +82,13 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null,
   });
-});
+};
+
+app.get('/api/v1/tours', getAllTours);
+app.get('/api/v1/tours/:id', getTourByID);
+app.post('/api/v1/tours', createTour);
+app.patch('/api/v1/tours/:id', updateTourByID);
+app.delete('/api/v1/tours/:id', deleteTourByID);
 
 const port = 3000;
 app.listen(port, '127.0.0.1', () => {
