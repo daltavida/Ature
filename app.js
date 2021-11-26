b75638dfcfe17b6452ee45f7a1ccd61de5db1b98
Ajay Singh <daltavida@gmail.com>
2021-11-26 18:07:00 +1030

    Use public dir for  for static files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
