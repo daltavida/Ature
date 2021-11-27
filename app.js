057d21366f380f8be3b17cf037175038600baaef
Ajay Singh <daltavida@gmail.com>
2021-11-27 18:07:00 +1030

    Change code based on enviornment variables

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const usersRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const app = require('./app');
 const dotenv = require('dotenv');
-
 dotenv.config({
   path: './config.env',
 });
 
-console.log(process.env);
+const app = require('./app');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, '127.0.0.1', () => {
   console.log(`App running on port ${port}`);
 });
