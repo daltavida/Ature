8e67e83c7a8ab7320de37fed362a4e06cf82621c
Ajay Singh <daltavida@gmail.com>
2021-12-09 18:07:00 +1030

    Add model for tours

--- server.js
+++ server.js
@@ -33,6 +33,22 @@ const tourSchema = new mongoose.Schema({
   },
 });
 
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, '127.0.0.1', () => {
   console.log(`App running on port ${port}`);
