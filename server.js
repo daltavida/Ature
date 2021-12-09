ac9ffc468a9730c77b8beb7e00440059ec9777fa
Ajay Singh <daltavida@gmail.com>
2021-12-09 18:07:00 +1030

    Separate tour model into file

--- server.js
+++ server.js
@@ -17,38 +17,6 @@ mongoose
     console.log('Connected to database');
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
 const port = process.env.PORT || 3000;
 app.listen(port, '127.0.0.1', () => {
   console.log(`App running on port ${port}`);
