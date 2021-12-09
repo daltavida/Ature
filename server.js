adb25256d583b52faceeb15a3524fbec5e0383fe
Ajay Singh <daltavida@gmail.com>
2021-12-09 18:07:00 +1030

    Create schema for tours

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({
   path: './config.env',
 });
 
 const app = require('./app');
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Connected to database');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, '127.0.0.1', () => {
