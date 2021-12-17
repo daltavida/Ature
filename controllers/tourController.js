389b7476fc6ed8a5aace8367af087ea4cfab77ac
Ajay Singh <daltavida@gmail.com>
2021-12-17 18:07:00 +1030

    Use database for update tour route

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -53,13 +53,25 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTourByID = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here>...',
-    },
-  });
+exports.updateTourByID = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data',
+    });
+  }
 };
 
 exports.deleteTourByID = (req, res) => {
