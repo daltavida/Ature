e09600597d8bd493e0551697d7ed3b35c80fa6b4
Ajay Singh <daltavida@gmail.com>
2021-12-19 18:07:00 +1030

    Create ratings and difficulty fields on  tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -14,9 +14,17 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a group size'],
   },
-  rating: {
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
