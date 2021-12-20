ae1a7c789f0c51709d133f577838d037798a0a5b
Ajay Singh <daltavida@gmail.com>
2021-12-20 18:07:00 +1030

    Update summary field

--- models/tourModel.js
+++ models/tourModel.js
@@ -34,6 +34,7 @@ const tourSchema = new mongoose.Schema({
   summary: {
     type: String,
     trim: true,
+    required: [true, 'A tour must have a description'],
   },
   description: {
     type: String,
