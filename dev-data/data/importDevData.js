e53f1e29aa5027c2a655af811dd7c3dc85545f28
Ajay Singh <daltavida@gmail.com>
2021-12-23 18:07:00 +1030

    Kill the process when finished

--- dev-data/data/importDevData.js
+++ dev-data/data/importDevData.js
@@ -25,6 +25,7 @@ const importData = async () => {
   try {
     await Tour.create(tours);
     console.log('Data successfully loaded');
+    process.exit();
   } catch (err) {
     console.log(err);
   }
@@ -34,6 +35,7 @@ const deleteData = async () => {
   try {
     await Tour.deleteMany();
     console.log('Data successfully deleted');
+    process.exit();
   } catch (err) {
     console.log(err);
   }
