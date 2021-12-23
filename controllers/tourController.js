a23741d18b7d61e5a4ffb589234f7923053646ac
Ajay Singh <daltavida@gmail.com>
2021-12-23 18:07:00 +1030

    Create query filter

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,7 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const tours = await Tour.find(req.query);
 
     res.status(200).json({
       status: 'success',
