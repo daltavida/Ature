375fa99190cb0fdfa4122db845e2b939d4e10f14
Ajay Singh <daltavida@gmail.com>
2021-12-29 18:07:00 +1030

    Create a featured route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,8 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
+router.route('/featured').get(tourController.getAllTours);
+
 router
   .route('/')
   .get(tourController.getAllTours)
