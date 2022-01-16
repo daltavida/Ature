4215031694685254b24b7814adff2fbcce1dfbb2
Ajay Singh <daltavida@gmail.com>
2022-01-16 14:00:00 +0100

    Create new error in bad route middleware

--- app.js
+++ app.js
@@ -17,16 +17,19 @@ app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
-  });
-  next();
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
 });
 
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-  res.status();
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
 });
 
 module.exports = app;
