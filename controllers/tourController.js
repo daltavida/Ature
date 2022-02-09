13f5ab1a7fcded65633a505300d3d8322da550c3
Ajay Singh <daltavida@gmail.com>
2022-02-09 14:00:00 +0100

    Create function to catch async errors

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -46,23 +46,20 @@ exports.getTourByID = async (req, res) => {
   }
 };
 
-exports.createTour = async (req, res) => {
-  try {
-    const newTour = await Tour.create(req.body);
-
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Invalid data',
-    });
-  }
+const catchAsync = (fn) => (req, res, next) => {
+  fn(req, res, next).catch(next);
 };
+
+exports.createTour = catchAsync(async (req, res) => {
+  const newTour = await Tour.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour,
+    },
+  });
+});
 
 exports.updateTourByID = async (req, res) => {
   try {
