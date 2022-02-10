934c2181bcf9dc153f6477a9237d1140c3de803f
Ajay Singh <daltavida@gmail.com>
2022-02-10 14:00:00 +0100

    Send 404 if ID not found

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.aliasFeatured = (req, res, next) => {
   req.query.limit = '5';
@@ -26,6 +27,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTourByID = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
 
   res.status(200).json({
     status: 'success',
