c88f64f2c8ed8fec4fae0be56be841da7fd738b1
Ajay Singh <daltavida@gmail.com>
2022-02-11 14:00:00 +0100

    Change error messages based on development environment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,20 @@
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      error: err,
+      stack: err.stack,
+    });
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
 };
