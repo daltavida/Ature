1f1e8bba5bd8fa1376642b2a3fc6597857273d45
Ajay Singh <daltavida@gmail.com>
2022-02-13 00:00:42 +1100

    Start listening and creating API

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server side');
+  res.status(200).json({
+    message: 'Hello from the server side!',
+    app: 'Natours',
+    author: 'Ajay Singh',
+  });
 });
 
 const port = 3000;
-app.listen(port, () => {
+app.listen(port, '127.0.0.1', () => {
   console.log(`App running on port ${port}`);
 });
