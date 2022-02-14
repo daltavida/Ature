a56470cbaeba9490d81c5cc2310d80d727dc11c5
Ajay Singh <daltavida@gmail.com>
2022-02-14 17:03:00 +0100

    Handle duplicate key errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,12 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = JSON.stringify(err.keyValue);
+  const message = `Duplicate field value for: ${value}. Please use another value`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -41,6 +47,9 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     if (err.name === 'CastError') {
       error = handleCastErrorDB(error);
+    }
+    if (err.code === 11000) {
+      error = handleDuplicateFieldsDB(error);
     }
     sendErrorProd(error, res);
   }
