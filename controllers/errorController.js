d3e38985bce934817b01ffedf9d11cbbeb224c65
Ajay Singh <daltavida@gmail.com>
2022-02-15 18:04:00 +0100

    Handle validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -11,6 +11,13 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  console.log(errors);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -50,6 +57,9 @@ module.exports = (err, req, res, next) => {
     }
     if (err.code === 11000) {
       error = handleDuplicateFieldsDB(error);
+    }
+    if (err.name === 'ValidationError') {
+      error = handleValidationErrorDB(error);
     }
     sendErrorProd(error, res);
   }
