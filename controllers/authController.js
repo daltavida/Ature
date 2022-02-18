aad1dc156734dd601dc3a0ae5c707c4b054d080d
Ajay Singh <daltavida@gmail.com>
2022-02-18 21:06:00 +0100

    Handle async errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
 
-exports.signup = async (req, res, next) => {
+exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
 
   res.status(201).json({
@@ -9,4 +10,4 @@ exports.signup = async (req, res, next) => {
       user: newUser,
     },
   });
-};
+});
