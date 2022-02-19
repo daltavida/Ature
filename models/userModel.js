2dc2536ae6f6c2ce0d8678726475d924446e4a16
Ajay Singh <daltavida@gmail.com>
2022-02-19 22:07:00 +0100

    Add missing next

--- models/userModel.js
+++ models/userModel.js
@@ -37,6 +37,7 @@ userSchema.pre('save', async function (next) {
 
   this.password = await bcrypt.hash(this.password, 12);
   this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
