6cdf12324b664417abca2cb2cf10c8ca1441f336
Ajay Singh <daltavida@gmail.com>
2022-02-19 22:07:00 +0100

    Create a pre save hook

--- models/userModel.js
+++ models/userModel.js
@@ -31,6 +31,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password')) return next();
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
