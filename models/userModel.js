b46c89d7e31b590b6f92cd78c9d78e7ba4334196
Ajay Singh <daltavida@gmail.com>
2022-02-19 22:07:00 +0100

    Delete password confirm after encryption

--- models/userModel.js
+++ models/userModel.js
@@ -32,10 +32,11 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre('save', function (next) {
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
 
-  this.password = bcrypt.hash(this.password, 12);
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
 });
 
 const User = mongoose.model('User', userSchema);
