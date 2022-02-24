ad287ee6a33e183075a8731586998188bb7b3cb8
Ajay Singh <daltavida@gmail.com>
2022-02-24 15:12:00 +0100

    Check if bearer token exists

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,5 +48,13 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
   next();
 });
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+
 const router = express.Router();
 
 router
