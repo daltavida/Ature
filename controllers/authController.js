479ed7fcda1a552a088329e6800bc653e87416d1
Ajay Singh <daltavida@gmail.com>
2022-02-25 16:13:00 +0100

    Handle user no longer exists error

--- controllers/authController.js
+++ controllers/authController.js
@@ -63,5 +63,10 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   const decoded = await promisify(jwt.verify(token, process.env.JWT_SECRET));
 
+  const freshUser = await User.findById(decoded.id);
+  if (!freshUser) {
+    return next(new AppError('The user no longer exists', 401));
+  }
+
   next();
 });
