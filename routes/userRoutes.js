f45dffbb39e33f94a9d9a46a46cc2ce647aa84c6
Ajay Singh <daltavida@gmail.com>
2022-03-04 23:20:00 +0100

    Fix wrong user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
-router.post('/forgotPassword', authController.signup);
-router.post('/resetPassword', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
+router.post('/resetPassword', authController.resetPassword);
 
 router
   .route('/')
